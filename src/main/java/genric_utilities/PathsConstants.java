3b84374a97ef9aa3ca14cab92752f7bdbfc747e4
User <User@DESKTOP-TG69BTG>
2025-12-30 19:03:16 +0530

    hvhv

--- src/main/java/genric_utilities/PathsConstants.java
+++ src/main/java/genric_utilities/PathsConstants.java
@@ -2,7 +2,7 @@ package genric_utilities;
 
 public interface PathsConstants 
 {
-	String propPath="./src/test/resources/files/commonfile.properties";
+	String propPath="./src/test/resources/LoginCredetials";
 	String excelPath ="./src/test/resources/files/Testdata.xlsx";
 	String v_url="jdbc:mysql://localhost:3307/raj";
 	String username="root";
